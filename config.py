42b3fff8fd06273ed87cb7379badbdd53d231a2a
juanlatorre <cattjuan@gmail.com>
2022-07-22 00:26:40 -0300

    flask Learning

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+SECRET_KEY = ''
+MYSQL_HOST = ""
+MYSQL_USER = ""
+MYSQL_PASSWORD = ""
+MYSQL_DB = ""
+MYSQL_PORT = 
+UPLOAD_PATH = os.path.dirname(os.path.abspath(__file__)) + '/uploads'
